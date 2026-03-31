a2f5016ffda5d1e814c4fb051584c3dc835cda7b
davedavedavedavedavedave <david.habereder@googlemail.com>
2026-03-31 23:10:36 +0200

    Update viewer_config.js

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
-const viewerConfig  = {
-    planetPreviewSources: {
-    nauvis: "../previews/nauvis.png",
-    vulcanus: "../previews/vulcanus.png",
-    gleba: "../previews/gleba.png",
-    fulgora: "../previews/fulgora.png",
-    aquilo: "../previews/aquilo.png"
+const viewerConfig = {
+  planetPreviewSources: {
+    nauvis: "https://dl.dropboxusercontent.com/scl/fi/ro0ayp0z0djwaa1dzu9r1/nauvis.png?rlkey=f1niskunmq8x6r2priqiryyvs",
+    vulcanus: "https://dl.dropboxusercontent.com/scl/fi/4cwdpnr004is7hjss38xb/vulcanus.png?rlkey=zlfeavp5uyw5z4iv6ofl42iwa",
+    gleba: "https://dl.dropboxusercontent.com/scl/fi/ntw2jbxw4conyftr7ohof/gleba.png?rlkey=23v2i7twsftgqx5g30kso3ktb",
+    fulgora: "https://dl.dropboxusercontent.com/scl/fi/uoj2d5pl642mn5hd8c5dk/fulgora.png?rlkey=31lurwch6lxsicujoe5rt8ds6",
+    aquilo: "https://dl.dropboxusercontent.com/scl/fi/z6rgm25kzjcypfsrbsa7r/aquilo.png?rlkey=y0aqm1iyry0mcmq6twsvrmr4w",
   },
-  planetNamesSource: "../previews/local_planet_names.js"
+  planetNamesSource: "https://dl.dropboxusercontent.com/scl/fi/6io3w7xmm0umuj093bzyv/remote_planet_names.json?rlkey=43hy19afy1jx10yeii5b8vssq"
 };
